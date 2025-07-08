9f4187ba2782c1ee90df0a261f08a618503c9345
egooda <katia.rlibanio@gmail.com>
2025-07-08 10:11:01 -0300

    descript

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function showNextSlide() {
 }
 
 // Muda a imagem automaticamente a cada 1 segundo
-let interval = setInterval(showNextSlide, 800);
+let interval = setInterval(showNextSlide, 600);
 
 // Pausa a animação no hover
 const slideshowContainer = document.querySelector('.slideshow-container');
@@ -22,5 +22,5 @@ slideshowContainer.addEventListener('mouseenter', () => {
     clearInterval(interval); // Pausa a troca de imagens
 });
 slideshowContainer.addEventListener('mouseleave', () => {
-    interval = setInterval(showNextSlide, 800); // Retorna a troca de imagens
+    interval = setInterval(showNextSlide, 600); // Retorna a troca de imagens
 });
